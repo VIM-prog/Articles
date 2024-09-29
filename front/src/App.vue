8c8f316bf6aead9dd78bf0c04bdcfcba1aceb234
asds32134 <evianiesian@list.ru>
2024-09-29 16:09:27 +0300

    TheThirdLevel_front
    Реализовать компонент добавления комментария

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <v-app>
     <v-main>
-      <ListComments/>
+      <CommentsForm/>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
 import ArticleForm from './components/ArticleForm.vue'; 
-import ListComments from './components/ListComments.vue'; 
+import ListComments from './components/ListComments.vue';
+import CommentsForm from './components/CommentsForm.vue';
 </script>
---
+++ front/src/components/CommentsForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-sheet class="mx-auto" width="500">
+      <form @submit.prevent="handleSubmit">
+        <v-container class="pa-md-12">
+          <div class="form-group">
+            <label for="content" style="text-align: center; display: block;">Комментарий:</label>  
+            <v-textarea
+              id="content"
+              class="mx-auto"
+              label="Content"
+              max-width="500"
+              persistent-placeholder
+              placeholder="Your text..."
+              variant="outlined"
+            />
+          </div>
+          <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+        </v-container>
+      </form>
+    </v-sheet>
+  </template>
+  
+  <script>
+  </script>
