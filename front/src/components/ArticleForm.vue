9f1075939f9b2edda3329b473fc49cc09c5b6386
asds32134 <evianiesian@list.ru>
2024-09-29 15:37:24 +0300

    TheFirstLevel_front
    Проинциализировать новое vuejs приложение, установить vue-router, vuex, axios остальное по желанию
    
    Реализовать компонент формы добавления/редактирования статьи

---
+++ front/src/components/ArticleForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-sheet class="mx-auto" width="500">
+    <form @submit.prevent="handleSubmit">
+      <v-container class="pa-md-12">
+        <div class="form-group">
+          <label for="title" style="text-align: center; display: block;">Заголовок:</label> 
+          <v-text-field
+           id="title"
+            class="mx-auto"
+            density="compact"
+            label="Title"
+            max-width="500"
+            persistent-placeholder
+            variant="outlined"
+          />
+        </div>
+        <div class="form-group">
+          <label for="content" style="text-align: center; display: block;">Содержание:</label>  
+          <v-textarea
+            id="content"
+            class="mx-auto"
+            label="Content"
+            max-width="500"
+            persistent-placeholder
+            placeholder="Your text..."
+            variant="outlined"
+          />
+        </div>
+        <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+      </v-container>
+    </form>
+  </v-sheet>
+</template>
+
+<script>
+</script>
